c2c6b34371a19c2dd090d5c0528d69014d7ed948
Staffan Åberg <staffan.aaberg@gmail.com>
2025-03-31 15:20:57 +0200

    First commit

---
+++ src/test/java/userTest/UserTest.java
@@ -0,0 +1,41 @@
+package userTest;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import user.User;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void testCreateUser() {
+        User user = new User("Staffan", "password123");
+    }
+
+    @Test
+    public void testGetUserName() {
+        User user = new User("Staffan", "password123");
+        String expected = "Staffan";
+        String actual = user.getUserName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetPassword() {
+        User user = new User("Staffan", "password123");
+        String expected = "password123";
+        String actual = user.getPassword();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetUserName() {
+        User user = new User("Staffan", "password123");
+        String expected = "Henry";
+        user.setUserName("Henry");
+        String actual = user.getUserName();
+        assertEquals(expected, actual);
+    }
+
+}
