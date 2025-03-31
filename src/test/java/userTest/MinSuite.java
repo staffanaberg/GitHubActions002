701ae2e23ad11b7e3144198f46501c64c199f7f4
Staffan Åberg <staffan.aaberg@gmail.com>
2025-03-31 16:21:12 +0200

    Hoppas att det funkar :)

--- src/test/java/userTest/MinSuite.java
+++
@@ -1,9 +0,0 @@
-package userTest;
-import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-@Suite
-//@SelectPackages("src.test.java.userTest")  // välj alla tester i detta paket
-@IncludeTags("myTest")             // kör bara tester med denna tagg
-public class MinSuite {
-}
